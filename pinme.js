251b14fa7c60305e1e5730df9f1c37a4aeedb451
charfweh <affan.ismail2016@gmail.com>
2020-05-02 16:06:14 +0530

    Initial bot commit

---
+++ pinme.js
@@ -0,0 +1,152 @@
+const botconfig = require("./config1.json" );
+const discord = require("../backup/node_modules/discord.js");
+const bot = new discord.Client({disableEveryone : true});
+const fs = require('fs')
+const prefix = botconfig.prefix;
+bot.on("ready", async ()=> {
+    console.log('wohoo i am ready to senpai!');
+    bot.user.setActivity("Pop-ups", {type : "PLAYING"});
+    bot.user.setStatus("dnd");
+});
+/*
+    TODOS
+    edit help command
+    make it more user friendly
+    add title
+    change desc
+*/
+bot.on("message", async message=> {
+
+    function reportdev(err,message){
+        const reportembed = new discord.RichEmbed()
+        .setTitle("Error encountered")
+        .addField("Server name",message.guild.name)
+        .addField("Server id",message.guild.id)
+        .addField("Error",err)
+        .setColor('RANDOM');
+        bot.users.get(botconfig.owner).send(reportembed);
+    }
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd  = args.shift().toLowerCase();
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    if(message.channel.type == 'dm') return
+    switch(cmd)
+    {   
+        case "set_bot":
+            let iid;
+            if(message.guild.channels.find(channel => channel.name === "pins")) return message.channel.send("Channel already exists.");
+            else{
+                let cat = await message.guild.createChannel("pinned archive",'category');
+                await message.guild.createChannel("pins",'text').then(c=>{
+                    c.setParent(cat.id)
+                    c.overwritePermissions(message.guild.id,{SEND_MESSAGES:false,READ_MESSAGES:true})
+                    c.overwritePermissions(message.guild.roles.find(r=>r.name == "Pin me"),{SEND_MESSAGES:true,READ_MESSAGES:true})
+                });
+                iid = message.guild.channels.find(channel => channel.name === "pins");
+                message.channel.send(`Channel created: <#${iid.id}>.`);
+                //message.author.send("Be sure to set parent of pins channel, i.e move channel to particular category.")
+            }
+            break;
+        
+        case "pins":
+        try{   
+            
+            if(!message.guild.channels.find(channel=> channel.name === "pins")) message.channel.send("Channel doesn't exist.");
+                else {
+                let val = [],authid = [],cont = [],avatar = [], channelname = [],url = [];
+                    await message.channel.fetchPinnedMessages() 
+                    .then(msg =>{
+                        msg.forEach(function(value, key){
+                        //if(cont.length==0){ cont.push("null")}
+                        cont.push(value);
+                        authid.push(value.author.id);
+                        val.push(value.author.username);
+                        channelname.push(value.channel.name);
+                        avatar.push(value.author.avatarURL);
+                        value.attachments.forEach(function(attachment){
+                                url.push(attachment.url);
+                            });
+                        });
+                if(val.length == 0) message.channel.send("No pins in this channel. Try pinning!");
+                else{
+                    for(i = 0 ; i < val.length; i++){
+                        const embed = new discord.RichEmbed()
+                        .setTitle("Pinned message")
+                        .addField("Author : "+val[i],"Content: "+cont[i])
+                        .setColor("RANDOM")
+                        .setThumbnail(avatar[i])
+                        .addField("Author id: ", authid[i])
+                        .addField("Url: ", url[i])
+                        .addField("Channel name: ", channelname[i]);
+                        message.guild.channels.find(channel=>{
+                            if(channel.name === "pins"){
+                                let iiid = channel.id;
+                                bot.channels.get(iiid).send(embed);
+                                } 
+                            })
+                        }
+                        message.channel.send("Pins loaded successfully!");
+                    }
+                })
+                .catch(console.error);
+            }
+        }catch(err){
+            reportdev(err,message);
+            message.channel.send("Sorry, I ran into an error, it'll be reported to developer")
+        }
+        break;
+        case "help":
+            if(!args[0]){
+            const helpembed = new discord.RichEmbed()
+            .setAuthor(message.guild.me.user.username,message.guild.me.user.avatarURL)
+            .setTimestamp()
+            .setColor("RANDOM")
+            .setThumbnail(message.author.avatarURL)
+            .setTitle("Pin-me Help")
+            .addField("**Available commands**",":warning: More commands will be added in the future")
+            .addField("``set_bot``","creates a channel for pinned messages logging with added permission\n It'll create a category named **``pinned archive``** and a ``pins`` channel\n**Permissions**\n It'll deny @everyone from sending messages")
+            .addField("``pins``","loads pinned messages of the channel in ``pins`` under **``pinned archive``** category")
+            .addField("``suggestion``","Your lovely suggestion will be sent to Bot's owner")
+            .addField("``ping``","Bot's latency")
+            .setFooter("For more, do [prefix] [command_name].")
+            .setDescription("Hello there, ever had urge to pin more messages after hitting the pin cap? Don't worry, I got this, you can safely log pinned message into a separate channel, giving you more space to pin~\nTo get started run ``~set_bot`` command");
+            message.channel.send(helpembed);
+            }
+        break;
+        case "suggestion":
+        if(message.channel.type!='dm')
+        message.channel.send("Command works in dm-only");
+        if(message.channel.type=='dm')
+        {
+                message.author.send("Please enter your suggestion, you've 1 minute");
+                const feedbackcollector = new discord.MessageCollector(message.channel, m=>m.author.id === message.author.id, {max:1,time:60000});
+                feedbackcollector.on('collect',feedcollect=>{
+                feedback = feedcollect.toString();
+                const feedembed = new discord.RichEmbed()
+                .setAuthor("Feedback from:")
+                .setColor("RANDOM")
+                .setThumbnail(message.author.avatarURL)
+                .addField(message.author.tag+"'s Feedback:",feedback)
+                .addField("Author's id:",message.author.id)
+                .setTimestamp()
+                bot.users.get('407170811787608064').send(feedembed);
+                message.author.send("Your suggestion has been sent, Thank you.");
+               
+            })
+                feedbackcollector.on('end',msg=>{
+                    message.author.send("Command timed out.");
+                })
+        }
+       
+        break;
+        case "ping":
+                const m = await message.channel.send("Ping?");
+                m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(bot.ping)}ms`);
+        break;
+        
+    }
+
+});
+
+bot.login(botconfig.token);
+
