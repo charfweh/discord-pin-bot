73e0e5914e6fb6a1a60422da82ec44b158eff79f
charfweh <affan.ismail2016@gmail.com>
2020-05-17 20:32:17 +0530

    Stable release 1.0

--- pinme.js
+++ pinme.js
@@ -135,8 +135,8 @@ bot.on("message", async message=> {
             .setThumbnail(message.author.avatarURL)
             .setTitle("Pin-me Help")
             .addField("**Available commands**",":warning: More commands will be added in the future")
-            .addField(":pushpin: ``set_bot``","creates a channel for pinned messages logging with added permission\n It'll create a category named **``pinned archive``** and a ``pins`` channel\n**Permissions for channel**\n It'll deny @everyone from sending messages\n**Permission needed:**\n> Administrator")
-            .addField(":pushpin: ``pins``","loads pinned messages of the channel in ``pins`` under **``pinned archive``** category\n**Embed Info**\nAuthor: Message's author\nContent:Message content\nAuthor id:Message's author id\nUrl: Url for attachments\nChannel name:Pinned message's channel name\n**Permission needed:**\n> Administrator")
+            .addField(":pushpin: ``set_bot``","creates a channel for pinned messages logging with added permission\n It'll create a category named **``pinned archive``** and a ``pins`` channel\n\n**Permissions for channel**\n It'll deny @everyone from sending messages\n\n**Permission needed:**\n> Administrator")
+            .addField(":pushpin: ``pins``","loads pinned messages of the channel in ``pins`` under **``pinned archive``** category\n\n**Embed Info**\nAuthor: Message's author\nContent:Message content\nAuthor id:Message's author id\nUrl: Url for attachments\nChannel name:Pinned message's channel name\n\n**Permission needed:**\n> Administrator")
             .addField(":pushpin: ``ping``","Bot's latency")
             .setFooter("For more, do [prefix] [command_name].")
             .setDescription("Hello there, ever had urge to pin more messages after hitting the pin cap? Don't worry, I got this, you can safely log pinned message into a separate channel, giving you more space to pin~\nTo get started run ``~set_bot`` command");
