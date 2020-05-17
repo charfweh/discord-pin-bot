c1ad0d1dc81fb0ab85ce5a95dd74356920f08f5d
carefree <44266892+charfweh@users.noreply.github.com>
2020-05-17 16:42:13 +0530

    Update pinme.js

--- pinme.js
+++ pinme.js
@@ -1,7 +1,7 @@
 const discord = require("discord.js");
 const bot = new discord.Client({disableEveryone : true});
 const owner= process.env.owner
-const cmdfile = require('cmds.json')
+const cmdfile = require('bot_config.json')
 const fs = require('fs')
 const talked = new Set();
 const prefix = process.env.prefix
