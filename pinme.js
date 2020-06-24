948f552e8b3c0f65e0b37cedb68af95f12ed6628
charfweh <affan.ismail2016@gmail.com>
2020-06-24 14:23:38 +0530

    help cmd fixed

--- pinme.js
+++ pinme.js
@@ -126,7 +126,7 @@ bot.on("message", async message=> {
     }
         catch(err){
             reportdev(err,message);
-            message.channel.send("Sorry, I ran into an error, error logs will be sent to developer")
+            message.channel.send(":X: Sorry, I ran into an error, error logs will be sent to developer")
         }
         break;
         case "help":
@@ -137,7 +137,7 @@ bot.on("message", async message=> {
             .setTitle("Pin-me Help")
             .addField("**Available commands**",":warning: More commands will be added in the future")
             .addField(":pushpin: ``set_bot``","creates a channel for pinned messages logging with added permission\n It'll create a category named **``pinned archive``** and a ``pins`` channel\n\n**Permissions for channel**\n It'll deny @everyone from sending messages\n\n**Permission needed:**\n> Administrator")
-            .addField(":pushpin: ``pins``","loads pinned messages of the channel in ``pins`` under **``pinned archive``** category\n\n**Embed Info**\nAuthor: Message's author\nContent:Message content\nAuthor id:Message's author id\nUrl: Url for attachments\nChannel name:Pinned message's channel name\n\n**Permission needed:**\n> Administrator")
+            .addField(":pushpin: ``pins``","loads pinned messages of the channel in ``pins`` under **``pinned archive``** category\n\n**Embed Info**\nAuthor: *Message's author*\nContent: *Message content*\nAuthor id: *Message's author id*\nUrl: *Url for attachments*\nChannel name: *Pinned message's channel name*\n\n**Permission needed:**\n> Administrator")
             .addField(":pushpin: ``ping``","Bot's latency")
             .addField(":pushpin: ``invite``","Invite link for the bot")
             .setDescription("Hello there, ever had urge to pin more messages after hitting the pin cap? Don't worry, I got this, you can safely log pinned message into a separate channel, giving you more space to pin~\nTo get started run ``~set_bot`` command")
